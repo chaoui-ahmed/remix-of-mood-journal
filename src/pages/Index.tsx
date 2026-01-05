e27ce520a2b51cd3bf5acc68f7353330047f9b3d
chaoui-ahmed <tahachaoui5@gmail.com>
2026-01-05 11:29:25 +0100

    feat(wording): update homepage subtitle to 'COUCOUUUU'

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -44,7 +44,7 @@ export default function Index() {
               Mes Pixels ✨
             </h1>
             <p className="text-muted-foreground font-medium italic">
-               Chaque carré est un souvenir. Remplis-les de couleurs ! 🎨<p className="text-muted-foreground mb-8">hihiiii</p>
+               Chaque carré est un souvenir. Remplis-les de couleurs ! 🎨<p className="text-muted-foreground mb-8">COUCOUUUU</p>
             </p>
           </div>
           
