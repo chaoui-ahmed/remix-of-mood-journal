0c0157ae7b0a9b6f5d10c602bffb3a341892bf3e
chaoui-ahmed <tahachaoui5@gmail.com>
2026-01-05 22:18:52 +0100

    feat(wording): update homepage subtitle to 'ton futur mari a cook'

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -44,7 +44,7 @@ export default function Index() {
               Mes Pixels ✨
             </h1>
             <p className="text-muted-foreground font-medium italic">
-               ✨✨✨✨✨<p className="text-muted-foreground mb-8">ENFINNNN LE PREMIER</p>
+               ✨✨✨✨✨<p className="text-muted-foreground mb-8">ton futur mari a cook</p>
             </p>
           </div>
           
