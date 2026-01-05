60107676dfb035e86384147b9f39d1ce5e1dc115
chaoui-ahmed <tahachaoui5@gmail.com>
2026-01-05 11:28:27 +0100

    feat(wording): update homepage subtitle to 'hihiiii'

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -44,7 +44,7 @@ export default function Index() {
               Mes Pixels ✨
             </h1>
             <p className="text-muted-foreground font-medium italic">
-               Chaque carré est un souvenir. Remplis-les de couleurs ! 🎨<p className="text-muted-foreground mb-8">hihiii</p>
+               Chaque carré est un souvenir. Remplis-les de couleurs ! 🎨<p className="text-muted-foreground mb-8">hihiiii</p>
             </p>
           </div>
           
