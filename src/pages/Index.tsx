f6ec41bf2908403756fca82c89aadd2615232038
chaoui-ahmed <tahachaoui5@gmail.com>
2026-01-12 22:49:56 +0100

    feat(wording): update homepage subtitle to '💀❤️'

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -44,7 +44,7 @@ export default function Index() {
               Mes Pixels ✨
             </h1>
             <p className="text-muted-foreground font-medium italic">
-               ✨✨✨✨✨<p className="text-muted-foreground mb-8">Bonne révisions ❤️</p>
+               ✨✨✨✨✨<p className="text-muted-foreground mb-8">💀❤️</p>
             </p>
           </div>
           
